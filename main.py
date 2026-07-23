99e8ca53b926c0945b392c45e269e4c62ee18837
mahmoudabdrabou636-lgtm <mahmoudabdrabou636@gmail.com>
2026-07-23 10:50:46 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from config import PROJECT_NAME
+
+def main():
+    print("=" * 40)
+    print(PROJECT_NAME)
+    print("Football Prediction System Started")
+    print("=" * 40)
+
+if __name__ == "__main__":
+    main()
